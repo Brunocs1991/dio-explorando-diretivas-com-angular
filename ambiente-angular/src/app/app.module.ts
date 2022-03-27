b622b41984987f29f306e6ffc753484f5345ecde
Bruno <brunocosta.1991@hotmail.com>
2022-03-27 20:06:47 -0300

    Criando directive

--- ambiente-angular/src/app/app.module.ts
+++ ambiente-angular/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MarcaTextoDirective } from './directives/marca-texto.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MarcaTextoDirective],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ ambiente-angular/src/app/directives/marca-texto.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appMarcaTexto]',
+})
+export class MarcaTextoDirective implements OnInit {
+  @Input() corDeFundo: string = 'yellow';
+  @Input() corDoTexto: string = 'white';
+
+  constructor(private elemento: ElementRef) {}
+
+  ngOnInit(): void {
+    this.mudarFundo;
+  }
+
+  private mudarFundo(cor: string = 'yellow') {
+    this.elemento.nativeElement.style.backgroundColor = this.corDeFundo || cor;
+    this.elemento.nativeElement.style.color = this.corDoTexto;
+    this.elemento.nativeElement.style.fontWeight = 'bold';
+  }
+}
